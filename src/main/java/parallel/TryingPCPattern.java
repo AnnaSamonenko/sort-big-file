25ff2b377ca7471216a9d08b5166086925d306b2
AnnaSamonenko <KarlMarks31>
2018-08-23 18:21:11 +0300

    Initial changes

---
+++ src/main/java/parallel/TryingPCPattern.java
@@ -0,0 +1,25 @@
+package parallel;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class TryingPCPattern {
+
+    private BlockingQueue blockingQueue = new ArrayBlockingQueue(10);
+
+    public TryingPCPattern() {
+    }
+
+    public void runDivideAndSortInParallel() {
+
+    }
+
+    private void producer() {
+
+    }
+
+    private void customer() {
+
+    }
+
+}
